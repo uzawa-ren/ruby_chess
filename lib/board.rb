5b1da1fd05e2d496e4b259f55717bbf4643a45ab
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-03-18 19:54:22 +0200

    Move all piece files to separate folder

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 require 'colorize'
-require_relative 'bishop'
-require_relative 'king'
-require_relative 'knight'
-require_relative 'pawn'
-require_relative 'queen'
-require_relative 'rook'
+require_relative 'pieces/bishop'
+require_relative 'pieces/king'
+require_relative 'pieces/knight'
+require_relative 'pieces/pawn'
+require_relative 'pieces/queen'
+require_relative 'pieces/rook'
 require_relative 'display'
 require_relative 'moves_finding'
 
